2b2e5c8c47bd8172bf53a9ba530476b7c8c3dec9
Your Name <you@example.com>
2020-06-10 13:59:41 +0800

    6-10 easyexcel

--- src/main/java/com/jiuyv/controller/ExcelController.java
+++ src/main/java/com/jiuyv/controller/ExcelController.java
@@ -2,9 +2,15 @@ package com.jiuyv.controller;
 
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.support.ExcelTypeEnum;
+import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.metadata.style.WriteFont;
+import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
+import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy;
 import com.jiuyv.excel.DemoDataOne;
 import com.jiuyv.excel.ExcelFillCellMergeStrategy;
 
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +22,7 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 
 @Controller
@@ -32,14 +39,63 @@ public class ExcelController {
         response.setCharacterEncoding("utf-8");
         String filename = "src/main/resources/write1.xlsx";
         response.setHeader("Content-disposition","attachment;filename="+filename);
-        //需要合并的列
-        int[] mergeColumeIndex = {0,2};
-        //需要从第一行开始，列头第一行
-        int mergeRowIndex = 1;
-        EasyExcel//将数据映射到DownloadDTO实体类并响应到浏览器
+        
+     // 头的策略
+        WriteCellStyle headWriteCellStyle = new WriteCellStyle();
+        // 背景设置为红色
+        headWriteCellStyle.setFillForegroundColor(IndexedColors.RED.getIndex());
+        WriteFont headWriteFont = new WriteFont();
+        headWriteFont.setFontHeightInPoints((short)20);
+        headWriteCellStyle.setWriteFont(headWriteFont);
+        // 内容的策略
+        WriteCellStyle contentWriteCellStyle = new WriteCellStyle();
+        // 这里需要指定 FillPatternType 为FillPatternType.SOLID_FOREGROUND 不然无法显示背景颜色.头默认了 FillPatternType所以可以不指定
+        contentWriteCellStyle.setFillPatternType(FillPatternType.SOLID_FOREGROUND);
+        // 背景绿色
+        contentWriteCellStyle.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+        WriteFont contentWriteFont = new WriteFont();
+        // 字体大小
+        contentWriteFont.setFontHeightInPoints((short)20);
+        contentWriteCellStyle.setWriteFont(contentWriteFont);
+        // 这个策略是 头是头的样式 内容是内容的样式 其他的策略可以自己实现
+        HorizontalCellStyleStrategy horizontalCellStyleStrategy =
+            new HorizontalCellStyleStrategy(headWriteCellStyle, contentWriteCellStyle);
+        // 需要合并的列
+        int[] mergeColumeIndex = {0,3};
+        // 需要从第一行开始，列头第一行
+        int mergeRowIndex = 5;
+        EasyExcel// 将数据映射到DownloadDTO实体类并响应到浏览器
                 .write(filename, DemoDataOne.class)
-                //07的excel版本,节省内存
+                // 07的excel版本,节省内存
                 //.excelType(ExcelTypeEnum.XLSX)
+                // 是否自动关闭输入流
+                .autoCloseStream(Boolean.TRUE)
+                // 设置样式
+                //.registerWriteHandler(horizontalCellStyleStrategy)
+                // 设置需要合并的行和列
+                .registerWriteHandler(new ExcelFillCellMergeStrategy(mergeRowIndex, mergeColumeIndex))
+//               // 自定义列宽度，有数字会
+                .registerWriteHandler(new LongestMatchColumnWidthStyleStrategy())
+                //设置excel保护密码
+//                .password("123456")
+                .sheet().doWrite(data);
+    }
+    
+    // 用这个生成的文件会乱码
+    @RequestMapping(value = "getEXcelfromweb", method = RequestMethod.POST)
+    public void getEXcelfromweb( HttpServletResponse response) throws IOException {
+        //获取数据源
+        List<DemoDataOne> data = data();
+        //设置输入流，设置响应域
+        response.setContentType("application/ms-excel");
+         String  fileName = "ceshi.xls";
+        response.setHeader("Content-disposition","attachment;filename="+fileName);
+        //需要合并的列
+        int[] mergeColumeIndex = {0, 1, 2};
+        //需要从第一行开始，列头第一行
+        int mergeRowIndex = 5;
+        EasyExcel//将数据映射到DownloadDTO实体类并响应到浏览器
+                .write(new BufferedOutputStream(response.getOutputStream()), DemoDataOne.class)
                 //是否自动关闭输入流
                 .autoCloseStream(Boolean.TRUE)
                 .registerWriteHandler(new ExcelFillCellMergeStrategy(mergeRowIndex, mergeColumeIndex))
@@ -57,6 +113,7 @@ public class ExcelController {
             data.setString("字符串");
             data.setDate(new Date());
             data.setDoubleData(0.56);
+            data.setId(UUID.randomUUID().toString().replaceAll("-",""));
             list.add(data);
         }
         return list;
--- src/main/java/com/jiuyv/excel/DemoDataOne.java
+++ src/main/java/com/jiuyv/excel/DemoDataOne.java
@@ -2,6 +2,7 @@ package com.jiuyv.excel;
 
 import com.alibaba.excel.annotation.ExcelIgnore;
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
 import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import com.alibaba.excel.annotation.write.style.ContentRowHeight;
 import com.alibaba.excel.annotation.write.style.HeadRowHeight;
@@ -14,21 +15,33 @@ import java.util.Date;
 @ColumnWidth(20)
 public class DemoDataOne {
 	
+	// @ColumnWidth(70)
     @ExcelProperty("字符串标题")
     private String string;
     
+    // @ColumnWidth(70)
+    @DateTimeFormat("yyyy年MM月dd日HH时mm分ss秒")
     @ExcelProperty("日期标题")
     private Date date;
     
+    // @ColumnWidth(70)
+    @ExcelProperty("id")
+    private String id;
+    
       // 当前列的宽度
-    @ColumnWidth(70)    
+    // @ColumnWidth(70)    
     @ExcelProperty("数字标题")
     private Double doubleData;
+    
+    
+    
     /**
      * 忽略这个字段
      */
     @ExcelIgnore
     private String ignore;
+    
+    
 	public String getString() {
 		return string;
 	}
@@ -52,6 +65,12 @@ public class DemoDataOne {
 	}
 	public void setIgnore(String ignore) {
 		this.ignore = ignore;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
 	}
     
 }
